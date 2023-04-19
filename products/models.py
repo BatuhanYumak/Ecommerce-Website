0d6947efdbd81028a9d214e55f22595a67417d40
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2023-04-19 15:29:00 +0200

    nieuwe DB

--- products/models.py
+++ products/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Product(models.Model):
-    product_name = models.CharField(max_length=100)
-    product_price = models.CharField(max_length=100)
-    product_photo = models.ImageField(upload_to='product_photo/', null=True, blank=True)
