0142c514d47e58abad2dd9f945376a3b37aa1388
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2023-04-20 15:37:36 +0200

    Product toegevoegd aan Tabel via admin paneel

--- products/models.py
+++ products/models.py
@@ -6,3 +6,4 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.CharField(max_length=100)
     photo = models.ImageField(upload_to='photo/', null=True, blank=True)
+    
