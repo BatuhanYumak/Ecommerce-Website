ddda1353063e3af73eb967ceac30b0a930bca427
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2023-04-22 21:52:02 +0200

    Ik heb app genaamd products een url.py bestand aangemaakt om de error te voorkomen

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render, redirect
+from .models import Product
+        
+
+def console(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'console.html', context)
