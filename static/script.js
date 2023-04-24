d690ce364f103233a59f3a05ead1224f8eb8e4b6
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2023-04-24 14:53:57 +0200

    Ik probeer een winkelwagen te maken

--- static/script.js
+++ static/script.js
@@ -6,4 +6,59 @@ function toggleDropdown() {
     } else {
       dropdown.style.display = "none";
     }
+  }let cartItems = [];
+
+function toggleCart() {
+  const cartDropdown = document.getElementById("cart-dropdown");
+  cartDropdown.style.display = cartDropdown.style.display === "none" ? "block" : "none";
+}
+
+function addItemToCart(item) {
+  // Check if item is already in cart
+  for (let i = 0; i < cartItems.length; i++) {
+    if (cartItems[i].id === item.id) {
+      cartItems[i].quantity++;
+      updateCart();
+      return;
+    }
   }
+
+  // If item is not in cart, add it
+  cartItems.push({
+    id: item.id,
+    name: item.name,
+    price: item.price,
+    quantity: 1
+  });
+
+  updateCart();
+}
+
+function updateCart() {
+  const cartCount = document.querySelector(".cart-count");
+  const cartItemsList = document.getElementById("cart-items");
+  const cartTotalPrice = document.getElementById("cart-total-price");
+  let total = 0;
+  let count = 0;
+
+  // Update cart count
+  cartCount.innerHTML = cartItems.reduce((acc, item) => {
+    return acc + item.quantity;
+  }, 0);
+
+  // Update cart items
+  cartItems.forEach(item => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `${item.quantity}x ${item.name} - $${item.price * item.quantity}`;
+    cartItemsList.appendChild(listItem);
+    total += item.price * item.quantity;
+    count += item.quantity;
+  });
+
+  // Update cart total price
+  cartTotalPrice.innerHTML = `$${total.toFixed(2)}`;
+
+  // Update cart empty message
+  const cartEmptyMessage = document.getElementById("cart-empty-message");
+  cartEmptyMessage.style.display = cartItems.length === 0 ? "block" : "none";
+}
