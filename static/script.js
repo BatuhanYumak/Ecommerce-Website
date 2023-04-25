626aed6432b522cc420b9c65b8a6273643d639dc
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2023-04-25 10:35:21 +0200

    Test winkelwagen

--- static/script.js
+++ static/script.js
@@ -6,59 +6,33 @@ function toggleDropdown() {
     } else {
       dropdown.style.display = "none";
     }
-  }let cartItems = [];
-
-function toggleCart() {
-  const cartDropdown = document.getElementById("cart-dropdown");
-  cartDropdown.style.display = cartDropdown.style.display === "none" ? "block" : "none";
-}
-
-function addItemToCart(item) {
-  // Check if item is already in cart
-  for (let i = 0; i < cartItems.length; i++) {
-    if (cartItems[i].id === item.id) {
-      cartItems[i].quantity++;
-      updateCart();
-      return;
-    }
   }
 
-  // If item is not in cart, add it
-  cartItems.push({
-    id: item.id,
-    name: item.name,
-    price: item.price,
-    quantity: 1
+
+  // Define an empty shopping cart object
+let cart = {};
+
+// Get all the "Add to Cart" buttons on the page
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+// Add an event listener to each button
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    // Get the product information from the button's data attributes
+    const productName = event.target.dataset.productName;
+    const productImage = event.target.parentElement.querySelector('img').src;
+    const productPrice = 100; // Replace with actual price
+
+    // Add the product to the cart object
+    if (cart[productName]) {
+      cart[productName].quantity++;
+    } else {
+      cart[productName] = {
+        image: productImage,
+        price: productPrice,
+        quantity: 1
+      };
+    }
   });
-
-  updateCart();
-}
-
-function updateCart() {
-  const cartCount = document.querySelector(".cart-count");
-  const cartItemsList = document.getElementById("cart-items");
-  const cartTotalPrice = document.getElementById("cart-total-price");
-  let total = 0;
-  let count = 0;
-
-  // Update cart count
-  cartCount.innerHTML = cartItems.reduce((acc, item) => {
-    return acc + item.quantity;
-  }, 0);
-
-  // Update cart items
-  cartItems.forEach(item => {
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `${item.quantity}x ${item.name} - $${item.price * item.quantity}`;
-    cartItemsList.appendChild(listItem);
-    total += item.price * item.quantity;
-    count += item.quantity;
-  });
-
-  // Update cart total price
-  cartTotalPrice.innerHTML = `$${total.toFixed(2)}`;
-
-  // Update cart empty message
-  const cartEmptyMessage = document.getElementById("cart-empty-message");
-  cartEmptyMessage.style.display = cartItems.length === 0 ? "block" : "none";
-}
+});
+    // Update 
