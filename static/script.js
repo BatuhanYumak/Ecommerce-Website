4d8f369dfa3c5974caa23c86eef9d6ac0b6ee33c
Batuhan Yumak <102596050+BatuhanYumak@users.noreply.github.com>
2023-04-25 12:06:41 +0200

    Shopping card werkt niet

--- static/script.js
+++ static/script.js
@@ -9,30 +9,3 @@ function toggleDropdown() {
   }
 
 
-  // Define an empty shopping cart object
-let cart = {};
-
-// Get all the "Add to Cart" buttons on the page
-const addToCartButtons = document.querySelectorAll('.add-to-cart');
-
-// Add an event listener to each button
-addToCartButtons.forEach(button => {
-  button.addEventListener('click', (event) => {
-    // Get the product information from the button's data attributes
-    const productName = event.target.dataset.productName;
-    const productImage = event.target.parentElement.querySelector('img').src;
-    const productPrice = 100; // Replace with actual price
-
-    // Add the product to the cart object
-    if (cart[productName]) {
-      cart[productName].quantity++;
-    } else {
-      cart[productName] = {
-        image: productImage,
-        price: productPrice,
-        quantity: 1
-      };
-    }
-  });
-});
-    // Update 
